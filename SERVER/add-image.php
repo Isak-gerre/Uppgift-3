7d62fb10bf32c58a3de88542ff9048a8b896b1ec
nstavnjak <niklasstavnjak@gmail.com>
2021-11-16 15:55:14 +0100

    Files/comments

---
+++ SERVER/add-image.php
@@ -0,0 +1,4 @@
+<!-- 
+    POST
+    •Kunna skapa en entitet. Ni ska kontrollera att alla fält existerar och inte är tomma. Skulle något saknas ska ni svara med något relevant meddelande så att användaren av ert API förstår vad som gått fel. Glöm inte att tänka på eventuella relationer som måste inkluderas i användarens förfrågan.
+-->
---
+++ SERVER/patch-images.php
@@ -0,0 +1,4 @@
+<!-- 
+    PATCH
+    •Kunna redigera en entitet baserat på ett ID. Ni ska kontrollera att dom fält som dom skickar med inte är tomma samt att ID:et dom specificerat faktiskt existerar. Skulle något gå fel ska ni svara med något relevant meddelande så att användaren av ert API förstår vad som gått fel. 
+-->
