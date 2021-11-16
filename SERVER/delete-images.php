672ab6a92c8f1a19c73f477485a980805378c01e
Isak <isak.gerre@gmail.com>
2021-11-16 15:50:59 +0100

    Update delete-images.php

---
+++ SERVER/delete-images.php
@@ -0,0 +1,3 @@
+<!-- 
+•DELETE
+•Kunna radera en entitet baserat på ett ID. Ni ska kontrollera att ID:et dom specificerat faktiskt existerar. Skulle något gå fel ska ni svara med något relevant meddelande så att användaren av ert API förstår vad som gått fel. -->
