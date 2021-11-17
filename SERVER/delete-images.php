61e1eae7e8b347009f698563c08ff794e8bb8c6a
nstavnjak <niklasstavnjak@gmail.com>
2021-11-17 16:15:54 +0100

    update delete

--- SERVER/delete-images.php
+++ SERVER/delete-images.php
@@ -7,9 +7,6 @@
     
     $users = json_decode(file_get_contents("DATABAS/users.json"), true);
     $posts = json_decode(file_get_contents("DATABAS/posts.json"), true);
-    
-    // $users = loadJSON("DATABAS/users.json");
-    // $posts = loadJSON("DATABAS/posts.json");
 
     // Vilken HTTP metod vi tog emot plus CONTENT TYPE
     $method = $_SERVER["REQUEST_METHOD"];
@@ -21,51 +18,60 @@
     $data = file_get_contents("php://input");
     $requestData = json_decode($data, true);
 
-    // Tar bort bild från mapp plus databas
-
     // Tar emot { id } och sedan raderar en användare baserat på `id`
     // Skickar tillbaka { id }
     if ($method === "DELETE") {
-        // Kontrollera att vi har den datan vi behöver
-        if (!isset($requestData["postID"], $requestData["userID"])) {
-            send(
-                [
-                    "code" => 1,
-                    "message" => "Missing `id` of request body"
-                ],
-                400
-            );
-        }
-
+        // Värden som skickat med metoden
         $postID = $requestData["postID"];
         $userID = $requestData["userID"];
 
-        
-        // Radera en bild från image-mappen
-        $image_url = $posts["posts"][$postID]["image_url"];
-        $http_host = $_SERVER["HTTP_HOST"];
-        inspect($image_url);
-        // $image_url = "http://localhost:7000/IMAGES/jEffrey.png";
-        // $directory = str_replace("http://$http_host/SERVER", "", $image_url);
+        // Kontrollera att vi har den datan vi behöver
+        if (isset($requestData["postID"], $requestData["userID"])) {
+            // Kontrollerar om nyckeln finns
+            if (array_key_exists($postID, $posts["posts"])){
+                // Radera en bild från image-mappen
+                $image_url = $posts["posts"][$postID]["image_url"];
+                $http_host = $_SERVER["HTTP_HOST"];
+                $directory = str_replace("http://$http_host/", "", $image_url);
+                unlink($directory);
 
-        // inspect($_SERVER);
+                // Raderar postens ID från avändarens array av posts
+                $arrayPosts = $users["users"][$userID]["posts"];
+                foreach($arrayPosts as $index => $post){
+                    if($post == $postID){
+                        array_slice($users["users"][$userID]["posts"], $index, 1);
+                        break;
+                    }
+                }
+                
+                // Raderar en post från databasen
+                unset($posts["posts"][$postID]);
+                
+                // Uppdaterar filen
+                saveJSON("users.json", $users);
+                saveJSON("posts.json", $posts);
+                // send(["id" => $id]);
 
-        // inspect($directory);
-        // unlink($directory);
-
-        // Raderar en post 
-        // unset($posts["posts"][$postID]);
-
-
-       
-
-        // Uppdatera filen
-        // saveJSON("posts.json", $posts);
-        // send(["id" => $id]);
+            } else {
+                $message = [
+                    "code" => 3,
+                    "message" => "Id dosn¨t exist"
+                ];
+                send($message, 404);
+            }
+        } else {
+            $message = [
+                "code" => 2, 
+                "message" => "Missing `id` of request body"
+            ];
+            send($message, 400);
+        }
 
     } else {
-        $json = json_encode(["message" => "Method Not Allowed"]);      
-        send($json, 405);
+        $message = [
+            "code" => 1,
+            "message" => "Method Not Allowed"
+        ];    
+        send($message, 405);
     }
-
 ?>
