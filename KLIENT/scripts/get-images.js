b56b23c65cc4031d2af88b03937ea8fb90ced4bb
Isak <isak.gerre@gmail.com>
2021-11-19 10:55:15 +0100

    cors

--- KLIENT/scripts/get-images.js
+++ KLIENT/scripts/get-images.js
@@ -1,1 +1,18 @@
 "use strict";
+
+async function getImages() {
+  try {
+    let response = await fetch("http://localhost:4000/get-images.php", {
+      credentials: "include",
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    let data = await response.json();
+    console.log(data.posts);
+    return data.posts;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getImages();
